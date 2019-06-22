fb4a2b17457b20a15c90aac905680e68f9096ebf
Andrey Malikov <a.malikov.int@gmail.com>
2019-06-22 12:02:42 +0300

    Выполнено задание 1

--- 2l_Malikov_Andrey.playground/Contents.swift
+++ 2l_Malikov_Andrey.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var str = "Hello, playground"
+// 1. Написать функцию, которая определяет, четное число или нет.
+
+func isItEvenNumber(n: Int)->Bool{
+    if n % 2 == 0 {
+        print("\(n) - четное число")
+        return true
+    }
+    else {
+        print("\(n) - нечетное число")
+        return false
+    }
+}
+var check: Bool
+check = isItEvenNumber(n: 4)
+check = isItEvenNumber(n: 5)
+
+
